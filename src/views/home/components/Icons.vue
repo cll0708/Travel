79bb921f68fb0deca21c1664a4eaf5c5c2412fc0
陈乐乐 <cll174281632@163.com>
2019-06-24 20:22:45 +0800

    add icons

---
+++ src/views/home/components/Icons.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page,index) in pages" :kay="index">
+        <div class="icon" v-for="item in page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" alt="">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomeIcons",
+    data () {
+      return {
+        iconList:[{
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+          desc: '景点门票'
+        },{
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png',
+          desc: '滑雪季'
+        },{
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png',
+          desc: '泡温泉'
+        },{
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/d0/e09575e66f4aa402.png',
+          desc: '游乐园'
+        },{
+          id: '0005',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+          desc: '景点门票'
+        },{
+          id: '0006',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png',
+          desc: '滑雪季'
+        },{
+          id: '0007',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png',
+          desc: '泡温泉'
+        },{
+          id: '0008',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/d0/e09575e66f4aa402.png',
+          desc: '游乐园'
+        },{
+          id: '0009',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/d0/e09575e66f4aa402.png',
+          desc: '游乐园'
+        }]
+      }
+    },
+    computed:{
+      pages(){
+        let pages = [];
+        this.iconList.forEach((item,index)=>{
+          let page = Math.floor(index/8);
+          if(!pages[page]){
+            pages[page] = [];
+          }
+          pages[page].push(item)
+        });
+        return pages
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/varibles.styl'
+  @import '~@/assets/styles/mixins.styl'
+  .icons >>> .swiper-container
+     height: 0
+     padding-bottom: 50%
+  .icon
+    position: relative
+    overflow: hidden
+    width: 25%
+    height: 0
+    float: left
+    padding-bottom: 25%
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      box-sizing: border-box
+      padding: .1rem
+      .icon-img-content
+        display: block
+        margin: 0 auto
+        height: 100%
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height: .44rem
+      line-height: .44rem
+      text-align: center
+      color: $darkTextColor
+      ellipse()
+</style>
--- src/views/home/components/Swiper.vue
+++ src/views/home/components/Swiper.vue
@@ -21,14 +21,17 @@
           loop: true
         },
         swiperList:[{
-          id:'0001',
-          imgUrl:'http://img1.imgtn.bdimg.com/it/u=3893146502,314297687&fm=26&gp=0.jpg'
+          "id": "0001",
+          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg"
         },{
-          id:'0002',
-          imgUrl:'http://img5.imgtn.bdimg.com/it/u=8685675,2171717858&fm=26&gp=0.jpg'
+          "id": "0002",
+          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg"
         },{
-          id:'0003',
-          imgUrl:'http://img1.imgtn.bdimg.com/it/u=1435544959,881026782&fm=26&gp=0.jpg'
+          "id": "0003",
+          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg"
+        },{
+          "id": "0004",
+          "imgUrl": "http://img1.qunarzz.com/piao/fusion/1712/91/a275569091681d02.jpg_640x200_0519ccb9.jpg"
         }]
       }
     }
@@ -42,7 +45,7 @@
     overflow: hidden
     width: 100%
     height:0
-    padding-bottom:56.28%
+    padding-bottom:31.25%
     background:#ccc
    .swiper-img
       width: 100%
