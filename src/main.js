f19075b955f7e9fb5c23afe6aa56f5ed322c2c68
陈乐乐 <cll174281632@163.com>
2019-06-24 17:51:14 +0800

    swiper

--- src/main.js
+++ src/main.js
@@ -6,9 +6,14 @@ import fastClick from 'fastclick'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
+
 
 new Vue({
   router,
---
+++ src/views/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "HomeSwiper",
+    data () {
+      return{
+        swiperOption: {
+          pagination:'.swiper-pagination',
+          loop: true
+        },
+        swiperList:[{
+          id:'0001',
+          imgUrl:'http://img1.imgtn.bdimg.com/it/u=3893146502,314297687&fm=26&gp=0.jpg'
+        },{
+          id:'0002',
+          imgUrl:'http://img5.imgtn.bdimg.com/it/u=8685675,2171717858&fm=26&gp=0.jpg'
+        },{
+          id:'0003',
+          imgUrl:'http://img1.imgtn.bdimg.com/it/u=1435544959,881026782&fm=26&gp=0.jpg'
+        }]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background-color: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height:0
+    padding-bottom:56.28%
+    background:#ccc
+   .swiper-img
+      width: 100%
+
+</style>
