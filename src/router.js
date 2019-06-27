c0eb7e58cd8c8c717054bf10bd28d0a1db640765
陈乐乐 <cll174281632@163.com>
2019-06-27 14:32:13 +0800

    fixed: router.js

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/views/home/Home.vue'
-import City from '@/views/city/City.vue'
+// import Home from '@/views/home/Home.vue'
+// import City from '@/views/city/City.vue'
 
 Vue.use(Router)
 
