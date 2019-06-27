3ffa23c18fc5bdf59c4759edfa39237b465e44d1
陈乐乐 <cll174281632@163.com>
2019-06-27 17:48:31 +0800

    city ajax

--- src/views/city/City.vue
+++ src/views/city/City.vue
@@ -2,12 +2,13 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :cities="cities" :hot="hotCities"></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
   import CityHeader from './components/Header'
   import CitySearch from './components/Search'
   import CityList from './components/List'
@@ -19,6 +20,30 @@
       CitySearch,
       CityList,
       CityAlphabet
+    },
+    data(){
+      return{
+        cities:{},
+        hotCities:[]
+      }
+    },
+    methods:{
+      getCityInfo(){
+        axios.get('/mock/city.json').then((res)=> {
+          //console.log(res)
+          res = res.data
+          console.log(res)
+          if(res.ret && res.data){
+            const { cities, hotCities } = res.data;
+            this.cities = cities;
+            this.hotCities = hotCities;
+
+          }
+        })
+      }
+    },
+    mounted(){
+      this.getCityInfo()
     }
   }
 </script>
